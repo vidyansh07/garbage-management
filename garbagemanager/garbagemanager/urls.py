5244981c7473192b78c5a81c13025c5e3d27f56d
Vidyansh07 <Vidyansht@gmail.com>
2024-07-27 17:09:10 +0530

    adding the report urls.py file.

--- garbagemanager/garbagemanager/urls.py
+++ garbagemanager/garbagemanager/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    
-    path('', include('users.urls')), # include your app urls.py here
+    path('', include('users.urls'), name= 'users'), # include your app urls.py here
+    path('report/', include('reports.urls'), name = 'reports'), # include your app urls.py here
+    # path('/collectors', include('collectors.urls'), name = 'collectors')
 ]
--- garbagemanager/reports/urls.py
+++ garbagemanager/reports/urls.py
@@ -4,5 +4,4 @@ from .views import *
 urlpatterns = [
     path('submit/', submit_report, name='submit_report'),
     path('form/', report_form, name='report_form'),
-    
 ]
